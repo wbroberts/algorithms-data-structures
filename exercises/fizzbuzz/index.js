774d551ffd853af4df5fae7a6cb8f4a82450b76c
William Roberts <willbarronroberts@gmail.com>
2019-10-18 16:06:25 -0600

    finished fizzbuzz

--- exercises/fizzbuzz/index.js
+++ exercises/fizzbuzz/index.js
@@ -5,13 +5,28 @@
 // of five print “buzz”. For numbers which are multiples
 // of both three and five print “fizzbuzz”.
 // --- Example
-//   fizzBuzz(5);
+  fizzBuzz(5);
 //   1
 //   2
 //   fizz
 //   4
 //   buzz
 
-function fizzBuzz(n) {}
+function fizzBuzz(n) {
+  for (let i = 1; i <=n; i++) {
+    const byThree = i % 3 === 0 ? 'fizz' : null;
+    const byFive = i % 5 === 0 ? 'buzz' : null;
+    
+    if (byThree && byFive) {
+      console.log(`${byThree}${byFive}`);
+    } else if (byFive) {
+      console.log(byFive);
+    } else if (byThree) {
+      console.log(byThree);
+    } else {
+      console.log(i);
+    }
+  }
+}
 
 module.exports = fizzBuzz;
