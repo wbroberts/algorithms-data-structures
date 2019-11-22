0c22e235080e36e40cb531b144a5b2f38080f93d
William Roberts <willbarronroberts@gmail.com>
2019-11-22 15:42:32 -0700

    finished queue with mutable and immutable versions

--- exercises/queue/index.js
+++ exercises/queue/index.js
@@ -8,6 +8,36 @@
 //     q.add(1);
 //     q.remove(); // returns 1;
 
-class Queue {}
+// class Queue {
+//   constructor() {
+//     this.queue = [];
+//   }
+
+//   add(data) {
+//     this.queue.push(data);
+//   }
+
+//   remove() {
+//     return this.queue.shift();
+//   }
+// }
+
+// Immutable implementation
+class Queue {
+  constructor() {
+    this.queue = [];
+  }
+
+  add(data) {
+    this.queue = [...this.queue, data];
+  }
+
+  remove() {
+    const firstItem = this.queue.shift();
+    this.queue = [...this.queue];
+    
+    return firstItem;
+  }
+}
 
 module.exports = Queue;
