facec766d49effb721110b2b8ebc7ce3a8102c9d
William Roberts <willbarronroberts@gmail.com>
2019-11-22 15:30:56 -0700

    finished anagrams

--- exercises/anagrams/index.js
+++ exercises/anagrams/index.js
@@ -4,10 +4,38 @@
 // in the same quantity. Only consider characters, not spaces
 // or punctuation.  Consider capital letters to be the same as lower case
 // --- Examples
-//   anagrams('rail safety', 'fairy tales') --> True
-//   anagrams('RAIL! SAFETY!', 'fairy tales') --> True
-//   anagrams('Hi there', 'Bye there') --> False
 
-function anagrams(stringA, stringB) {}
+function anagrams(stringA, stringB) {
+  const a = toMap(stringA);
+  const b = toMap(stringB);
+
+  if (length(a) !== length(b)) {
+    return false;
+  }
+
+  return isMatch(a, b);
+}
+
+function toMap(str) {
+  const map = {};
+  
+  str
+    .replace(/[^\w]/gi, '')
+    .toLowerCase()
+    .split('')
+    .forEach(char => {
+      map[char] ? map[char]++ : map[char] = 1;
+    });
+
+  return map;
+}
+
+function length(obj) {
+  return Object.keys(obj).length;
+}
+
+function isMatch(obj1, obj2) {
+  return Object.keys(obj1).every(key => obj1[key] === obj2[key]);
+}
 
 module.exports = anagrams;
