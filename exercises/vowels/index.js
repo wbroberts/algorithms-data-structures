bdd70a0bc615e28dc488ef95a3a6e6b2bc7a3805
William Roberts <willbarronroberts@gmail.com>
2020-01-28 16:02:39 -0700

    completed vowels with two implementations

--- exercises/vowels/index.js
+++ exercises/vowels/index.js
@@ -5,8 +5,43 @@
 // --- Examples
 //   vowels('Hi There!') --> 3
 //   vowels('Why do you ask?') --> 4
-//   vowels('Why?') --> 0
+//   vowels("Why?") --> 0
 
-function vowels(str) {}
+function vowels(str) {
+  return str.split("").filter(char => isVowel(char)).length;
+}
+
+function isVowel(char) {
+  return char.toLowerCase().match(/([aeiou])/);
+}
+
+// function vowels(str) {
+//   let count = 0;
+
+//   for (let i = 0; i < str.length; i++) {
+//     if (isVowel(str.charAt(i))) {
+//       count++;
+//     }
+//   }
+
+//   return count;
+// }
+
+// function isVowel(char) {
+//   switch (char.toLowerCase()) {
+//     case "a":
+//       return true;
+//     case "e":
+//       return true;
+//     case "i":
+//       return true;
+//     case "o":
+//       return true;
+//     case "u":
+//       return true;
+//     default:
+//       return false;
+//   }
+// }
 
 module.exports = vowels;
