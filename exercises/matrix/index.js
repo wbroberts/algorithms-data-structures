90b474d67ebcd67cc5cfa78ff03c8c05b79718f4
William Roberts <willbarronroberts@gmail.com>
2020-02-09 22:17:12 -0700

    finished matrix

--- exercises/matrix/index.js
+++ exercises/matrix/index.js
@@ -15,6 +15,58 @@
 //     [11, 16, 15, 6],
 //     [10,  9,  8, 7]]
 
-function matrix(n) {}
+function matrix(n) {
+  const matrix = [];
+
+  for (let i = 0; i < n; i++) {
+    matrix.push([]);
+  }
+
+  let top = 0;
+  let bottom = n - 1;
+  let left = 0;
+  let right = n - 1;
+
+  let count = 1;
+  let direction = "right";
+
+  while (count <= n * n) {
+    if (direction === "right") {
+      for (let i = left; i <= right; i++) {
+        matrix[top][i] = count;
+        count++;
+      }
+
+      top++;
+      direction = "down";
+    } else if (direction === "down") {
+      for (let i = top; i <= bottom; i++) {
+        matrix[i][right] = count;
+        count++;
+      }
+
+      right -= 1;
+      direction = "left";
+    } else if (direction === "left") {
+      for (let i = right; i >= left; i--) {
+        matrix[bottom][i] = count;
+        count++;
+      }
+
+      bottom -= 1;
+      direction = "up";
+    } else if (direction === "up") {
+      for (let i = bottom; i >= top; i--) {
+        matrix[i][left] = count;
+        count++;
+      }
+
+      left += 1;
+      direction = "right";
+    }
+  }
+
+  return matrix;
+}
 
 module.exports = matrix;
